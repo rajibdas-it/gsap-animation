011a89f01c166e2afb3187370e81f92c37187aa8
rajibdas-it <rajibrad@gmail.com>
2023-04-13 14:49:29 +0600

    create logo like my website software enginner

--- gasp-animation/script.js
+++ gasp-animation/script.js
@@ -15,9 +15,34 @@ tl.fromTo(
     borderRadius: "1.5rem",
     delay: 0.25,
     duration: 2.5,
-    ease: "slow(0.2,0.7,false)",
+    ease: "elastic.out(1.5,2)",
   }
 );
+
+tl.fromTo(".cta1", { x: "100%", opacity: 0.5 }, { x: 0, opacity: 1 }, "<30%");
+tl.fromTo(".cta2", { y: "-100%", opacity: 0.5 }, { y: 0, opacity: 1 }, "<30%");
+tl.fromTo(".cta3", { x: "-100%", opacity: 0.5 }, { x: 0, opacity: 1 }, "<30%");
+tl.fromTo(".cta4", { x: "100%", opacity: 0.5 }, { x: 0, opacity: 1 }, "<30%");
+tl.fromTo(".cta5", { y: "100%", opacity: 0.5 }, { y: 0, opacity: 1 }, "<30%");
+tl.fromTo(".cta6", { x: "100%", opacity: 0.5 }, { x: 0, opacity: 1 }, "<30%");
+// tl.fromTo(
+//   ".btn",
+//   { y: "20", opacity: 0.5, display: "none" },
+//   { y: 0, opacity: 1, display: "block" },
+//   "<"
+// );
+tl.fromTo(".btn", { y: "20", opacity: 0 }, { y: 0, opacity: 1 }, "<");
+// tl.fromTo(
+//   ".logo",
+//   { scale: 1.5, borderRadius: "0rem" },
+//   {
+//     scale: 1,
+//     borderRadius: "1.5rem",
+//     delay: 0.25,
+//     duration: 2.5,
+//     ease: "elastic.out(1.5,3)",
+//   }
+// );
 
 // tl.fromTo(
 //   ".hero-img",
@@ -42,3 +67,26 @@ tl.fromTo(
 //     ease: "elastic.out(1.5,2)",
 //   }
 // );
+
+const logo = document.querySelector(".logo");
+const letters = logo.textContent.split("");
+logo.textContent = "";
+// console.log(letter);
+letters.forEach((letter) => {
+  logo.innerHTML += `<span class="letter">${letter}</span>`;
+});
+
+//ektar por ekta letter ashar jonno stagger user korbo tailei oirkhm ekta por ekta letter ashbe
+gsap.set(".letter", { display: "inline-block" });
+gsap.fromTo(
+  ".letter",
+  { x: "100%", display: "none" },
+  {
+    x: 0,
+    display: "inline-block",
+    delay: 1,
+    stagger: 0.3,
+    repeat: -1, //repeat:-1 dewa mane holo infinite loop hisabe choltei thakbe
+    ease: "back.out",
+  }
+);
